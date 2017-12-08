923be90e65c2b7eaa23d86dfd72b565e39a0a80a
harsh2g <harshgadani55@gmail.com>
2017-12-08 14:03:00 -0500

    Add files via upload

--- Text_Tokenizer_Analyzer.py
+++ Text_Tokenizer_Analyzer.py
@@ -1,5 +1,9 @@
 import re, time, string
 from collections import Counter
+from itertools import islice, tee
+from mpi4py import MPI #import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 #running on Python 3.6.2
 #used for getting runtime
 start_time = time.time()
@@ -16,9 +20,15 @@ with open('C:\\Users\\chi10\\OneDrive\\Documents\\test.txt', 'r') as f:
         if count == 1: #for counting "unique" words
             unique_count = unique_count + 1
     print("")
-    for i in words[:10]:
+
+    ##Prints out top 1000 most frequent words
+    print ("Top 1000 most frequent words")
+    print ("============================")
+    print ("============================")
+    for i, count in c.most_common(1000):
         print (i, count)
-    
+    ##
+        
     print("")
     
     print ("Number of unique words is: ", unique_count) #print number of unique words
@@ -33,15 +43,56 @@ words = str.split(data) #split words into array
 characters = 0 #hold number of characters
 for i in words:
     characters += len(i)
-
-##
-    
-print ("")
-print ("")
 print ("Number of characters in file: ", characters)
+##
+
 #Printing number of total words in file
 print ("Total number of words is: ", sum(len(line.split()) for line in open('C:\\Users\\chi10\\OneDrive\\Documents\\test.txt', 'r')))
 #Printing number of sentences in file
 print ("Total number of sentences is: ", open('C:\\Users\\chi10\\OneDrive\\Documents\\test.txt', 'r').read().count("."))
+print ("")
+
+def ngrams(arr, n): #function for get n-grams
+    teearr = arr
+    while True:
+        a, b = tee(teearr)
+        x = tuple(islice(a, n))
+        if len(x) == n:
+            yield x
+            next(b)
+            teearr = b
+        else:
+            break
+
+#Filter out the punctuation in text file to get just words
+justwords = [w.lower() for w in words if (w.isalnum()) and (w.find("'"))]
+
+##Top 1000 most frequent 2-word bigrams
+
+print ("Top 1000 most frequent 2-word bigrams")
+print ("=====================================")
+ngramprint = Counter(ngrams(justwords, 2)) #without punctuation
+for i, value in ngramprint.most_common(1000):
+    print (i, value)
+##
+print ("")
+##Top 1000 most frequent 3-word 3-grams
+print ("Top 1000 most frequent 3-word trigrams")
+print ("=====================================")
+ngramprint = Counter(ngrams(justwords, 3)) #without punctuation
+for i, value in ngramprint.most_common(1000):
+    print (i, value)
+##
+
+print ("")
+##Top 1000 most frequent n-word n-grams
+print ("Top 1000 most frequent n-word n-grams")
+print ("=====================================")
+ngramprint = Counter(ngrams(justwords, 4))
+for i, value in ngramprint.most_common(1000):
+    print (i, value)
+##
+print ("")
+
 #Printing runtime
 print ("Time to run program: ", (time.time() - start_time))
